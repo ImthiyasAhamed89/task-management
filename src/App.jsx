724a766abde4283bb53668cb5eb065c34399f6ff
imthiyas <imthiyas@astasystech.com>
2025-02-27 07:51:34 +0530

    task completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Container } from './components'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   
+      <Container>
+       Task Management App
+      </Container>
+  )
+}
+
+export default App
